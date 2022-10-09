1bb5e971708f449f1ffb35d3ce0b688443827a71
Hansol Cho <zwwz11@naver.com>
2022-10-09 16:50:19 +0900

    First Commit

---
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -0,0 +1,72 @@
+package org.zerock.b01.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@Log4j2
+public class SampleController {
+
+    class SampleDTO {
+        private String p1, p2, p3;
+
+        public String getP1() {
+            return p1;
+        }
+
+        public String getP2() {
+            return p2;
+        }
+
+        public String getP3() {
+            return p3;
+        }
+    }
+
+    @GetMapping("/hello")
+    public void hello(Model model) {
+        log.info("hello.............");
+        model.addAttribute("msg", "Hello World!");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model) {
+        log.info("ex/ex2..");
+
+        List<String> strList = IntStream.range(0, 10).mapToObj(i -> "Data" + i).collect(Collectors.toList());
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value -- p1";
+        sampleDTO.p2 = "Value -- p2";
+        sampleDTO.p3 = "Value -- p3";
+        model.addAttribute("dto", sampleDTO);
+    }
+
+    @GetMapping("/ex/ex3")
+    public void ex3(Model model) {
+        model.addAttribute("arr", new String[] { "AAA", "BBB", "CCC" });
+    }
+}
